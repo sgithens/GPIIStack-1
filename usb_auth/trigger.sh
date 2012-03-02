2804d037add61e8d94fc051112b99181da4a6145
Steven Githens <swgithen@mtu.edu>
2012-03-02 00:19:58 -0500

    Changes to push gpiiprefs to node

--- usb_auth/trigger.sh
+++ usb_auth/trigger.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This is a workaround for correct udev script execution.
 # There should be a way to avoid using a intermediate file.
-cd /home/boyan/repos/gpiistack/usb_auth
+cd /home/sgithens/code/GPIIStack-1/usb_auth
+echo "The stuff $1 $2" >> /tmp/gpiistack-1.txt
 ./usb_test.sh $1 $2 & exit
--- usb_auth/usb_test.sh
+++ usb_auth/usb_test.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 if [ $1 -eq 1 ]; then
+        echo "I am getting a status 1" >> /tmp/gpiistack-1.txt
+        echo "Args are: 1: $1  and 2: $2" >> /tmp/gpiistack-1.txt
 	# USB disk drive is added.
 	retries=0
 	# Wait for drive to be mounted. There should be a better solution using nautilus.
-	until [[ `mount | grep $2` || $retries -ge 5 ]]; do
-		retries=$[$retries+1]
-		sleep 1
-	done
-
+	# until [[ `mount | grep $2` || $retries -ge 5 ]]; do
+	# 	retries=$[$retries+1]
+	# 	sleep 1
+	# done
+        echo "About to sleep" >> /tmp/gpiistack-1.txt
+        sleep 5
+        echo "Done Sleeping  sleep" >> /tmp/gpiistack-1.txt
 	mountLocation=`mount | grep $2 |cut -d " " -f 3`
 	token=`cat $mountLocation/token.txt`
+        gpiiprefs=`cat $mountLocation/gpiiprefs.json`
+        echo "Out token is: $token" >> /tmp/gpiistack-1.txt
 	echo "User logged in on device $2 with token ${token}." >> log.txt
 	echo $2:$token >> users.txt						# Keep the location and token in a users file.
-	curl http://localhost:3000/user/$token
+#	echo $2:$token >> users.txt						# Keep the location and token in a users file.
+	#curl http://localhost:3000/user/$token
+        curl --upload-file $mountLocation/gpiiprefs.json http://localhost:3000/magsettings
+        echo "Done uploading file!" >> /tmp/gpiistack-1.txt
 else
 	# USB disk drive is removed.
 	token=`grep $2 < users.txt | cut -d ":" -f 2`
